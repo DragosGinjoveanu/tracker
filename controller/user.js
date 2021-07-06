83b7f186bb87851e6500cef18090ef318d38dee0
Dragos <ginjoveanu.dragos@gmail.com>
2021-07-06 13:45:07 +0300

    finished register functionality, implemented login route

--- controller/user.js
+++ controller/user.js
@@ -11,6 +11,18 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.post('/login', async function(req, res) {
+    var user = req.params.user;
+    var password = req.params.password;
+    try {
+        const a = await pool.query('SELECT name AS name, password AS password FROM users WHERE name = $1', [user]);
+        // if password = password - redirectionare home page
+        console.log(a);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
 router.post('/register', body('user').isLength({ min: 3 }), body('password').isLength({ min: 5 }), async function(req, res){
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -22,6 +34,7 @@ router.post('/register', body('user').isLength({ min: 3 }), body('password').isL
             var user = req.body.user;
             const paste = await pool.query("INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *", [user, password]);
             console.log('Account created.\nUsername: ' + user +', Password: '+ password);
+            res.redirect('http://localhost:3000/user/login');
         } catch (error) {
             console.log('User already exists.');
             res.render('loginFail', {fail: 'Registration failed!', location: '/user/register', error: 'Username already exists.'});
