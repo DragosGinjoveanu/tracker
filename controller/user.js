9bc4f36c180e7699b24671b5e3bec56984f1be77
Dragos <ginjoveanu.dragos@gmail.com>
2021-07-06 12:00:36 +0300

    modified register/login forms

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get('/register', (req, res) => {
     res.render('register');
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const user = require('./controller/user');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
 
