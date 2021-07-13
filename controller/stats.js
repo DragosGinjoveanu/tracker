586f7c5b80337e457fc1ff9cca71db7b359138dc
Dragos <ginjoveanu.dragos@gmail.com>
2021-07-13 21:04:53 +0300

    added folders and files for the stats functionality

---
+++ controller/stats.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const queries = require('../model/stats/queries');
+
+app.get('/', async function(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+module.exports = {router};
