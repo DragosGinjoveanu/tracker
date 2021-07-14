224a0f0779ea094d14b5515a90c0fc1825c86ae3
Dragos <ginjoveanu.dragos@gmail.com>
2021-07-14 17:56:49 +0300

    created functions to get journal pages and tasks

--- controller/stats.js
+++ controller/stats.js
@@ -5,7 +5,11 @@ const queries = require('../model/stats/queries');
 router.get('/', async function(req, res) {
     try {
         const points = await queries.getPoints(req.session.username);
-        res.render('userStats', {user: req.session.username, points: points.points});
+        const pages = await queries.getNrJournalPages(req.session.username);
+        const doneTasks = await queries.getNrTasks(req.session.username, true);
+        const stats = {points, pages, doneTasks};
+        console.log(stats);
+        res.render('userStats', {user: req.session.username, stats: stats});
     } catch (error) {
         console.log(error.message);
     }
--- model/stats/queries.js
+++ model/stats/queries.js
@@ -2,7 +2,17 @@ const pool = require('../database');
 
 async function getPoints(user) {
     const res = await pool.query('SELECT points as points FROM users WHERE name = $1', [user]);
-    return res.rows[0];
+    return res.rows[0].points;
+}
+
+async function getNrJournalPages(user) {
+    const res = await pool.query('SELECT COUNT(*) FROM journals WHERE name = $1', [user]);
+    return res.rows[0].count;
+}
+
+async function getNrTasks(user, status) {
+    const res = await pool.query('SELECT COUNT(*) FROM todos WHERE name = $1 AND done = $2', [user, status]);
+    return res.rows[0].count;
 }
 
 async function addPoints(user, points) {
@@ -13,5 +23,4 @@ async function removePoints(user, points) {
     await pool.query('UPDATE users SET points = points - $1 WHERE name = $2', [points, user]);
 }
 
-
-module.exports = {getPoints, removePoints, addPoints};
+module.exports = {getPoints, getNrJournalPages, getNrTasks, removePoints, addPoints};
