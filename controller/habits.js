3a0dcbf50aad8db710c47eb77bbff2b32d4a2138
Dragos <ginjoveanu.dragos@gmail.com>
2021-07-23 14:56:06 +0300

    removed unnecessary code

--- controller/habits.js
+++ controller/habits.js
@@ -4,9 +4,18 @@ const { body, validationResult } = require('express-validator');
 const queries = require('../model/habits/queries');
 const authentication = require('../helper/javascript/authentication');
 
-//for adding habit from list
-router.post('/create/:name', async function(req, res) {
-    console.log(req.params.name);
+//for adding habit from default list (home page)
+router.post('/create/:habit', async function(req, res) {
+    const user = req.session.username;
+    const habit = req.params.habit;
+    //label/no label? 
+    res.redirect('http://localhost:3000/habits');
+});
+
+//gets the user's habits (no labels)
+router.get('/', async function(req, res) {
+    const user = req.session.username;
+    var habits;
 });
 
 module.exports = router;
